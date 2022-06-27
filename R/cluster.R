061701299027f0bcb3e9d49d8c95836363c5c514
tremrs <108291912+tremrs@users.noreply.github.com>
2022-06-27 11:40:51 -0500

    added timeout argument

--- R/cluster.R
+++ R/cluster.R
@@ -10,8 +10,8 @@
 #' @examples
 #' cluster <- new_cluster(2)
 #' cluster
-new_cluster <- function(n) {
-  sessions <- replicate(n, callr::r_session$new(wait_timeout = 15 * 1000))
+new_cluster <- function(n, timeout) {
+  sessions <- replicate(n, callr::r_session$new(wait_timeout = timeout * 1000))
   structure(
     sessions,
     cleaner = Cleaner$new(),
